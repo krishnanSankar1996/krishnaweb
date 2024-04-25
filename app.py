943c6826ad35b958dfba89d80483cc41af75b0da
krishnanSankar1996 <168058461+krishnanSankar1996@users.noreply.github.com>
2024-04-25 12:13:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import seaborn as sns 
 
-st.title("Sales KPI Dashboard with nice charts")
+st.title("Krishna KPI Dashboard we app")
 
 st.markdown('## Key Metrics')
 
